b97405fbc40568926699513d949dc4b1ca808c3a
jimmybnguyen <jimmyn9@uw.edu>
2017-01-26 23:15:57 -0800

    add change color scriot

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public SteamVR_TrackedObject rightController;
+    public ColorManager cm;
+
+    void Update()
+    {
+    }
+    void OnTriggerStay(Collider col)
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)rightController.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GetComponent<Renderer>().material.color = cm.color;
+        }
+    }
+}
